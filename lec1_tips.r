adcc18391c5ac06eebd3186815a5898f64a2b181
Choi Seokjun <Choi.Seokjun@outlook.com>
2021-09-27 23:32:39 -0700

    test commit

---
+++ lec1_tips.r
@@ -0,0 +1,63 @@
+ls() #all objects
+rm(list=ls())
+
+x=c("hi",3,TRUE)
+x #all string
+x=c(3,"hi",TRUE)
+x
+is.character(x)
+
+y = vector("logical",3)
+print(y)
+y = vector("numeric",4)
+print(y)
+y = seq(1,8,by=2)
+print(y)
+y = 3:8
+print(y)
+y = rep(TRUE,6)
+print(y)
+y = rep(seq(1,3),2)
+print(y)
+y = rep(seq(1,3),each=2)
+print(y)
+
+is.numeric(y) #work to vectors
+is.logical(y)
+is.character(y)
+
+length(y)
+
+
+z = c(0,1,2,3,4,5)
+as.logical(z)
+
+z = c(NA, 4, 3, NA, 1)
+is.numeric(z)
+is.na(z)
+
+0/0 #NAN
+Inf #Inf
+
+w = c(1.3, -2.2, 1.15, 0.23, -1.1)
+sort(w)
+order(w)
+rank(w)
+?rank
+w[order(w)]
+w[rank(w)]
+
+#vectors
+#matrices
+
+S = matrix(c(1, 0.5, 1.3, 0.4, -1, 0.2, 0.5, -1, 0.6), 3, 3)
+S
+apply(S,2,sum) #col
+apply(S,1,sum) #row
+S
+S[,3]
+
+#solve
+solve(S) #inverse
+x = c(0,1,2)
+solve(S,x) # solution of Sb=x
