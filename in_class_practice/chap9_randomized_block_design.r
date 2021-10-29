ab1c380643f6c37857746608ef397207f9b4445f
Choi Seokjun <Choi.Seokjun@outlook.com>
2021-10-28 22:30:42 -0700

    2-way anova

--- in_class_practice/chap9_randomized_block_design.r
+++ in_class_practice/chap9_randomized_block_design.r
@@ -33,18 +33,61 @@ plot(CIs, las = 1)
 poison <- read.csv("Rx-data/poison.csv")
 
 #EDA
-interaction.plot(poison$Poison, poison$Treatment, response=poison$Time, lwd=2)
-interaction.plot(poison$Treatment, poison$Poison, response=poison$Time, lwd=2)
+interaction.plot(poison$Poison, poison$Treatment,
+    response = poison$Time, lwd = 2)
+interaction.plot(poison$Treatment, poison$Poison,
+    response = poison$Time, lwd = 2)
 #if parallel, then we can expect there is no interaction
 
 M <- aov(Time ~ Poison * Treatment, data = poison) # * model with interaction
 anova(M)
 #interaction is not significant
-
+#so, delete the interaction term.
 M1 <- aov(Time ~ Poison + Treatment, data = poison) # * model without interaction
 anova(M1)
 
 
+#using lm : 
 model_M <- lm(poison$Time ~ poison$Poison * poison$Treatment)
-summary(model_M)
+# y_{ijk} = \mu + \alpha_i + \beta_j + \gamma_{ij} + \epsilon_{ijk}
 #restriction: poison1, treatA, interaction(pois1,.), interation(.,treat=1) = 0
+
+anova(model_M)
+# F test for interaction
+# H0 : \gamma_{ij} = 0 for all i,j
+# H1 : At least one \gamma_{ij} \neq 0
+# and, if interaction effect is not significant,
+# H0 : \alpha_i = 0 for all i
+# H1 : At least \alpha_i \neq 0
+# and
+# H0 : \beta_j = 0 for all j
+# H1 : At least \beta_j \neq 0
+
+summary(model_M)
+#Again, restriction: poison1, treatA, interaction(pois1,.), interation(.,treat=1) = 0
+
+
+
+model_M1 <- lm(poison$Time ~ poison$Poison + poison$Treatment)
+anova(model_M1)
+summary(model_M1)
+
+diff_M1 <- TukeyHSD(M1, which = c("Poison", "Treatment")) #for aov object
+diff_M1
+par(mfrow = c(1, 2))
+plot(diff_M1)
+
+par(mfrow = c(2, 2))
+plot(model_M1)
+# become more variability as fitted value increase
+# 3 outliers -> not normal, skewed (actually, the data are about 'survival times'(>0))
+
+
+#transformation
+log_M1 <- lm(log(Time) ~ Poison + Treatment, data=poison)
+anova(aov(log_M1))
+plot(log_M1)
+
+reci_M1 <- lm(1/Time ~ Poison + Treatment, data=poison)
+anova(aov(reci_M1))
+plot(reci_M1)
